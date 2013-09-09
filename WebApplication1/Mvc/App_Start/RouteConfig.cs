414fbde183f1a3b4d7305d68363a65c026f6feb2
Xu YiJun <yijun.xu@chukou1.com>
2013-09-09 17:39:46 +0800

    Added Some Mvc Content

---
+++ WebApplication1/Mvc/App_Start/RouteConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebApplication1.MVC.App_Start
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("favicon.ico");
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("{resource}.aspx/{*pathInfo}");
+
+            routes.MapRoute(
+                "Default",
+                "Mvc/{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                new[] { "WebApplication1.Controllers" }
+                );
+        }
+    }
+}
