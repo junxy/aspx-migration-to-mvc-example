faeb99f979b95253053d909333bc6f765ea8a94d
Xu YiJun <yijun.xu@chukou1.com>
2013-09-09 17:44:20 +0800

    Added Mvc Config

---
+++ WebApplication1/Global.asax.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using WebApplication1.MVC.App_Start;
+
+namespace WebApplication1
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            InitConfig.RegisterMvc();
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+        }
+    }
+
+}
---
+++ WebApplication1/Mvc/App_Start/InitConfig.cs
@@ -0,0 +1,69 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebApplication1.MVC.App_Start
+{
+    public class InitConfig
+    {
+        public static void RegisterMvc()
+        {
+            //AreaRegistration.RegisterAllAreas();
+
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new CustomViewEngine());
+        }
+    }
+
+    public class CustomViewEngine : RazorViewEngine
+    {
+        private const string MasterLocationPrefix = "~/MVC/";
+        private const string AreaLocationPrefix = MasterLocationPrefix + "Areas/";
+
+        public CustomViewEngine()
+            : this(null)
+        {
+        }
+
+        public CustomViewEngine(IViewPageActivator viewPageActivator)
+            : base(viewPageActivator)
+        {
+            AreaViewLocationFormats = new[]
+            {
+                AreaLocationPrefix + "{2}/Views/{1}/{0}.cshtml",
+                AreaLocationPrefix + "{2}/Views/Shared/{0}.cshtml",
+            };
+            AreaMasterLocationFormats = new[]
+            {
+                AreaLocationPrefix + "{2}/Views/{1}/{0}.cshtml",
+                AreaLocationPrefix + "{2}/Views/Shared/{0}.cshtml",
+            };
+            AreaPartialViewLocationFormats = new[]
+            {
+                AreaLocationPrefix + "{2}/Views/{1}/{0}.cshtml",
+                AreaLocationPrefix + "{2}/Views/Shared/{0}.cshtml",
+            };
+
+            ViewLocationFormats = new[]
+            {
+                MasterLocationPrefix + "Views/{1}/{0}.cshtml",
+                MasterLocationPrefix + "Views/Shared/{0}.cshtml",
+            };
+            MasterLocationFormats = new[]
+            {
+                MasterLocationPrefix + "Views/{1}/{0}.cshtml",
+                MasterLocationPrefix + "Views/Shared/{0}.cshtml",
+            };
+            PartialViewLocationFormats = new[]
+            {
+                MasterLocationPrefix + "Views/{1}/{0}.cshtml",
+                MasterLocationPrefix + "Views/Shared/{0}.cshtml",
+            };
+
+            FileExtensions = new[]
+            {
+                "cshtml",
+            };
+        }
+    }
+}
